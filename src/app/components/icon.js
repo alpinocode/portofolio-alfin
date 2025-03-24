27547760d8a52e8fcfbbf95610b67ada49921f6e
GontaGantiPC <alfinohasan629@gmail.com>
2025-03-24 09:05:42 +0700

    update project

--- src/app/components/icon.js
+++ src/app/components/icon.js
@@ -5,15 +5,27 @@ import { useRouter } from "next/navigation";
 
 export default function Icon({ alt, src, toLink }) {
   const router = useRouter();
+
+  if (!src) {
+    console.error("Icon: 'src' is required");
+    return null;
+  }
+
   return (
-    <div>
+    <div className="cursor-pointer">
       <Image
         src={src}
         width={50}
         height={50}
-        alt={alt}
+        alt={alt || "icon"}
         className="rounded-full ml-6"
-        onClick={() => router.push(toLink)}
+        onClick={() => {
+          if (toLink) {
+            router.push(toLink);
+          } else {
+            console.warn("Icon: 'toLink' is not provided");
+          }
+        }}
       />
     </div>
   );
